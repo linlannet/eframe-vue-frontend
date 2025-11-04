06a45516824b95e23983296d8f65af4770b11494
lupeiran <lupeiran@wondersgroup.com>
2025-11-04 10:53:26 +0800

    登录页面调整

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,22 +1,36 @@
 <template>
 	<div class="login-container">
 		<div class="login-intro">
-			<h1>统一用户中心</h1>
-			<div class="desc">
-				提供统一的用户登录服务，支持多种登录方式，包括账号密码登录、手机验证码登录、第三方登录等。
+			<div class="background">
+
 			</div>
-			<div class="login-bg"><img src="@/assets/img/bg_login.png" alt="" /></div>
+			<div class="login-inner">
+				<div class="login-bg">
+					<LoginBg />
+				</div>
+				<h1>统一用户中后台管理系统</h1>
+				<div class="desc">
+					工程化、高性能、跨平台的一体化支撑平台
+				</div> 
+			</div>
+			
 		</div>
 		<div class="login-form">
-			<div class="login-title">
-				<el-button link :class="loginType === 'account' ? 'account' : ''" @click="loginSwitch('account')">登录</el-button>
-				<el-button link :class="loginType === 'mobile' ? 'account' : ''" @click="loginSwitch('mobile')">手机登录</el-button>
+			<div class="form-inner">
+				<div class="login-title">
+					<el-button link :class="loginType === 'account' ? 'account' : ''" @click="loginSwitch('account')">登录</el-button>
+					<el-button link :class="loginType === 'mobile' ? 'account' : ''" @click="loginSwitch('mobile')">手机登录</el-button>
+				</div>
+				<account v-if="loginType === 'account'" />
+				<mobile v-if="loginType === 'mobile'" />
+				<third />
 			</div>
-			<account v-if="loginType === 'account'" />
-			<mobile v-if="loginType === 'mobile'" />
-			<third />
+			<div class="copyright">
+				 Copyright © 2025-2035 EFRAME. All rights reserved.
+			</div>
 		</div>
 	</div>
+	<logo />
 </template>
 
 <script setup lang="ts">
@@ -26,6 +40,8 @@ import Mobile from './mobile-login.vue'
 import Third from './social-login.vue'
 import { useRoute } from 'vue-router'
 import cache from '../../utils/cache'
+import logo from './logo.vue'
+import LoginBg from './loginbg.vue'
 // 登录类型
 const loginType = ref('account')
 const loginSwitch = (type: string) => {
@@ -51,27 +67,59 @@ watch(
 <style lang="scss" scoped>
 .login-container {
 	display: flex;
-	justify-content: space-evenly;
 	align-items: center;
 	height: 100vh;
 }
 .login-intro {
 	display: flex;
+	position: relative;
 	flex-direction: column;
-	width: 520px;
+	justify-content: center;
+	width: 60%;
+	height: 100vh;
 	flex: 0 1 auto;
+	background: #f1f3f6;
+	overflow: hidden;
+	text-align: center;
+	
+	.background{
+		position: absolute;
+		width: 100%;
+		height: 100%;
+		background: linear-gradient(154deg, rgba(7, 7, 9, 0.08235) 30%, hsl(212 100% 45% / 30%) 48%, rgba(7, 7, 9, 0.08235) 64%);
+    	filter: blur(100px);
+	}
 }
 .login-intro h1 {
-	color: var(--el-color-primary);
+	margin: 50px 0 0 0;
+	font-size: 26px;
+	color: #333;
 }
 .login-intro .desc {
-	color: rgb(113, 115, 112);
+	color: #666;
 	line-height: 32px;
-	padding: 15px 0;
+	font-size: 16px;
 }
-.login-bg img {
-	width: 520px;
+.login-bg {
+	margin:auto;
+	width: 220px;
+	animation: float-enhanced 2.5s ease-in-out infinite;
 }
+@keyframes float-enhanced {
+  0% {
+    transform: translateY(0);
+    opacity: 0.8;
+  }
+  50% {
+    transform: translateY(-15px);
+    opacity: 1;
+  }
+  100% {
+    transform: translateY(0);
+    opacity: 0.8;
+  }
+}
+
 .login-title {
 	display: flex;
 	justify-content: space-around;
@@ -91,10 +139,11 @@ watch(
 	flex: 0 1 auto;
 	padding: 40px;
 	border-radius: 6px;
-	box-shadow: 1px 1px 8px #aaa6a6;
 	box-sizing: border-box;
-	width: 440px;
-
+	width: 40%;
+	.form-inner {
+		width: 100%;
+	}
 	:deep(.el-input) {
 		height: 45px;
 		margin-top: 5px;
@@ -106,11 +155,21 @@ watch(
 			font-size: 16px;
 		}
 	}
+	.copyright{
+		position: absolute;
+		right: 40px;
+		bottom: 20px;
+		text-align: right;
+		color: #999;
+	}
 }
 
 @media only screen and (max-width: 992px) {
 	.login-intro {
 		display: none;
+	}
+	.login-form{
+		width: 100%;
 	}
 }
 @media only screen and (max-width: 768px) {
