adb6c533bb71f704efe1402f47e7d3d25cfef687
linlaninfo <linlannet@163.com>
2025-11-11 14:05:01 +0800

    保留测试的路径配置

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,8 @@ export default ({ mode }) => {
       open: true,
       proxy: {
         '/dev_api': {
-          target: 'https://linlan.net/eframe_backend/',
+        //   target: 'https://linlan.net/eframe_backend/',
+          target: 'http://localhost:8081/eframe_backend/',
           changeOrigin: true,
           rewrite: (path) => path.replace(/^\/dev_api/, ''),
         },
